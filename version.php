f050e1d6aa1bb6c5a6908cfe36e0771f72bb305a
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2026-02-15 12:40:33 +0000

    V500.1.1

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025041800;
+$plugin->version = 2025041801;
 $plugin->requires = 2025041400.00; // 5.0 (Build: 20250414).
 $plugin->supported = [500, 500];
 $plugin->component = 'filter_synhi';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '500.1.0';
+$plugin->release = '500.1.1';
